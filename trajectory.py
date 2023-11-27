a98c2d169eccf98078718723300ef89bdf0d6298
Saran Sankar <epsaransankar@gmail.com>
2023-11-27 11:19:48 -0500

    Prepare data for training

--- trajectory.py
+++ trajectory.py
@@ -1,19 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_csv('data/eth/hotel/pixel_pos.csv')
 
-plt.figure(figsize=(10, 6))
+pedestrianId = pd.Series(data.columns).apply(lambda x: int(x.split('.')[0]))
+df = pd.DataFrame([data.iloc[0, :].T,
+                   data.iloc[1, :].T,
+                   data.iloc[2, :].T]).transpose()
+df['pedestrianId'] = list(pedestrianId)
+pedestrian_ids = df['pedestrianId'].unique()
 
-plt.plot(df[1][df['pedestrianId'] == 1],
-         df[2][df['pedestrianId'] == 1],
-         marker='o', linestyle='-')
+sequence_length = 3
+X, y = [], []
 
-# Add labels and legend
-plt.title('Plot of DataFrame Rows')
-plt.xlabel('Column')
-plt.ylabel('Values')
-plt.legend()
+for pedestrian_id in pedestrian_ids:
+    x_values = df[df['pedestrianId'] == pedestrian_id].drop(
+        columns=['pedestrianId']).values
+    for i in range(len(x_values) - sequence_length):
+        X.append(x_values[i:i+sequence_length])
+        y.append(x_values[i+sequence_length])
 
-# Show the plot
-plt.show()
+
+# Convert to numpy arrays
+X = np.array(X)
+y = np.array(y)
+
+# Normalize the data (optional but recommended)
+X = (X - X.min()) / (X.max() - X.min())
+y = (y - y.min()) / (y.max() - y.min())
+
+# Reshape the data for LSTM input (samples, time steps, features)
+X = X.reshape(X.shape[0], sequence_length, 3)
+
+# plt.figure(figsize=(10, 6))
+#
+# plt.plot(df[1][df['pedestrianId'] == 1],
+#          df[2][df['pedestrianId'] == 1],
+#          marker='o', linestyle='-')
+#
+# # Add labels and legend
+# plt.title('Plot of DataFrame Rows')
+# plt.xlabel('Column')
+# plt.ylabel('Values')
+# plt.legend()
+#
+# # Show the plot
+# plt.show()
