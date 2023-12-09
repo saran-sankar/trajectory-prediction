055917d51f27c651eb6e2d18d4d9b30e9ab70703
Saran Sankar <epsaransankar@gmail.com>
2023-12-08 19:48:20 -0500

    Add method to plot training loss

--- lstm_model.py
+++ lstm_model.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import History
 
 def build_lstm_model(sequence_length, num_features):
     model = Sequential()
@@ -12,7 +13,18 @@ def build_lstm_model(sequence_length, num_features):
     return model
 
 def train_model(model, X_train, y_train, epochs=100, batch_size=128, validation_split=0.1):
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+    history = History()  # Create a history object to store training metrics
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split, callbacks=[history])
+
+    # Plot training loss
+    plt.figure(figsize=(10, 6))
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Training and Validation Loss over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
 
 def make_predictions(model, X_test):
     return model.predict(X_test)
